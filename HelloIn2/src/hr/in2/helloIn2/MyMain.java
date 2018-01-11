6ae9c58dbb943346849d5a1638b1051389e0777f
tomislavpa <tomislavpa@N007702.in2.hr>
2018-01-12 00:34:46 +0100

    Vraćam na inicijalno stanje.

--- HelloIn2/src/hr/in2/helloIn2/MyMain.java
+++ HelloIn2/src/hr/in2/helloIn2/MyMain.java
@@ -4,11 +4,7 @@ public class MyMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Juhu :-)");
-		// pišem i pišem i opet pišem
-		
-		int i = 100;
-		i = 101;
+
 	}
 
 }
