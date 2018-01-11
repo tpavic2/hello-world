55fd8cf508014286324426867e69ac0497a72fe2
tomislavpa <tomislavpa@N007702.in2.hr>
2018-01-12 00:27:06 +0100

    Inicijalni commit.

---
+++ HelloIn2/src/hr/in2/helloIn2/MyMain.java
@@ -0,0 +1,11 @@
+package hr.in2.helloIn2;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Juhu :-)");
+		// pišem i pišem
+	}
+
+}
